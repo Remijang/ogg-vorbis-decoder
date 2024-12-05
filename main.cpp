061d6a6f8d0406b37546c7faef9cfff3b43c1d0d
b11902051 <b11902051@csie.ntu.edu.tw>
2024-12-06 03:26:40 +0800

    add mdct + overlapping, now debug io problem

--- main.cpp
+++ main.cpp
@@ -16,33 +16,37 @@ void parse(io_buf &in) {
 	comment		   cc;
 	setup		   ss;
 	for(int i = 0; i < 7; ++i) q.push_back(in.read_u(8));
-	while(1) {
+	int h_count = 0;
+	while(h_count < 3) {
 		if(q[1] == 'v' && q[2] == 'o' && q[3] == 'r' && q[4] == 'b' && q[5] == 'i' && q[6] == 's'){
 			cout << "abc" << " " << (int)q[0] << endl;
 			if(q[0] == 1) id.init(in);
 			if(q[0] == 3) cc.init(in);
 			if(q[0] == 5) ss.init(in, id);
-			q.clear();
-			for(int i = 0; i < 7; ++i) q.push_back(in.read_u(8));
-			continue;
+			h_count++;
+			if(h_count < 3) {
+				q.clear();
+				for(int i = 0; i < 7; ++i) q.push_back(in.read_u(8));
+				continue;
+			}
+			else break;
 		}
 		q.push_back(in.read_u(8));
 		q.erase(q.begin());
-		if(in.ptr >= 20000) break;
 	}
 	cout << cc.vender_string.data() << endl;
 	for(unsigned int i = 0; i < cc.user_comment_list_length; ++i){
 		cout << cc.user_comment_list[i].data() << endl;
 	}
-	packet p;
+	packet p(id);
 	int count = 0;
 	while(in.ptr < in.len) {
 		cout << count << endl;
 		p.decode(in, id, ss);
 		count++;
+		cout << in.ptr << " " << in.len << endl;
+		cout << in.offset << endl;
 	}
-	cout << in.ptr << " " << in.len << endl;
-	cout << in.offset << endl;
 }
 
 int main() {
@@ -55,11 +59,14 @@ int main() {
 	 * set io buffer
 	 */
 	struct stat st;
-	stat("test.ogg", &st);
+	stat("test2.ogg", &st);
 	int fin_siz = st.st_size;
-	unsigned char *in_map = (unsigned char*) mmap(NULL, fin_siz, PROT_READ | PROT_WRITE, MAP_SHARED, fin, 0);
-	io_buf input(in_map, fin_siz);
+	unsigned char *in_map = (unsigned char*) mmap(NULL, fin_siz, PROT_READ, MAP_SHARED, fin, 0);
+	{
+		io_buf input(in_map, fin_siz);
 
-	parse(input);
-
+		parse(input);
+	}
+	printf("%x\n", in_map);
+	munmap(in_map, fin_siz);
 }
